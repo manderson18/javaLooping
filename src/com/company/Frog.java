9dc17ce976951e80aec39c9f5a2c6f5ea24d26c1
manderson18 <ma128@pvsd.org>
2018-05-23 08:52:38 -0400

    Initial commit

---
+++ src/com/company/Frog.java
@@ -0,0 +1,10 @@
+package com.company;
+
+
+/**
+ * Created by ma128 on 4/25/18.
+ */
+public class Frog{
+
+    public String toString;
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> homeboys = new ArrayList<String>();
+        homeboys.add("Adam");
+        homeboys.add("Usain Bolt");
+        homeboys.add("Lowkey");
+        homeboys.add("Mr. Wednesday");
+
+        for (int i = 0; i <homeboys.size() ;i++){
+            System.out.println(homeboys.get(i) + " wus good fam?");
+
+
+
+        }
+
+
+    }
+
+}
